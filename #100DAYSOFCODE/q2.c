4f74c2590794367f9c7bb2e61d6c2a3e545f41ee
braghav730 <braghav730@gmail.com>
2025-08-26 14:13:39 +0530

    Add files via upload

---
+++ #100DAYSOFCODE/q2.c
@@ -0,0 +1,46 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+#include<stdio.h>
+int main(){
+    int a = 10;
+    int b = 5;
+    printf("The sum of two numbers is %d\n", (a+b));
+    printf("The difference of two numbers is %d\n", (a-b));
+    printf("The product of two numbers is %d\n",(a*b));
+    printf("The quotient of two numbers is %d\n", (a/b));
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ #100DAYSOFCODE/q4.c
@@ -0,0 +1,8 @@
+//Write a program to calculate the area and circumference of a circle given its radius.
+#include<stdio.h>
+int main(){
+float r = 10;
+    printf("The area of circle is %f\n", (3.14*r*r));
+    printf("The circumference of circle is %f\n", (2*3.14*r));
+    return 0;
+}
