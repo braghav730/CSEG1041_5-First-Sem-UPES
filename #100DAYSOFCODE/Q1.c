b0ce0202549b3b75faf5d8eb40de15574c7753f1
braghav730 <braghav730@gmail.com>
2025-08-26 14:12:47 +0530

    Create Q1.c

---
+++ #100DAYSOFCODE/Q1.c
@@ -0,0 +1,9 @@
+//Write a program to input two numbers and display their sum.
+#include<stdio.h>
+int main(){
+    int a = 3;
+    int b = 5;
+    printf("The sum of two numbers is %d", (a+b));
+    return 0;
+
+}
