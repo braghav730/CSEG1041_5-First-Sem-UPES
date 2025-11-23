3df78d42c676a41e451100aa8cc5db3c7e6dac42
braghav730 <braghav730@gmail.com>
2025-11-23 12:24:25 +0530

    Add files via upload

---
+++ q135.c
@@ -0,0 +1,25 @@
+//Assign explicit values starting from 10 and print them.
+#include <stdio.h>
+
+enum Days {
+    SUNDAY = 10,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+};
+
+int main() {
+
+    printf("SUNDAY = %d\n", SUNDAY);
+    printf("MONDAY = %d\n", MONDAY);
+    printf("TUESDAY = %d\n", TUESDAY);
+    printf("WEDNESDAY = %d\n", WEDNESDAY);
+    printf("THURSDAY = %d\n", THURSDAY);
+    printf("FRIDAY = %d\n", FRIDAY);
+    printf("SATURDAY = %d\n", SATURDAY);
+
+    return 0;
+}
